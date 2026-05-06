e92a559d0a370bb257ae84d8de746582bf8923aa
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-05-06 23:24:56 +0000

    Rewrite fuzzer to use oss-fuzz approach: remove deleted guidovranken repos

--- mayhem/build.sh
+++ mayhem/build.sh
@@ -12,60 +12,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-################################################################################
 
 export ASAN_OPTIONS=alloc_dealloc_mismatch=0
 
-if [[ $CFLAGS = *sanitize=address* ]]
-then
+if [[ $CFLAGS = *sanitize=address* ]]; then
     export CXXFLAGS="$CXXFLAGS -DASAN"
 fi
-
-if [[ $CFLAGS = *sanitize=memory* ]]
-then
+if [[ $CFLAGS = *sanitize=memory* ]]; then
     export CXXFLAGS="$CXXFLAGS -DMSAN"
 fi
 
-if [[ $SANITIZER = coverage ]]
-then
-    export CXXFLAGS="$CXXFLAGS -fno-use-cxa-atexit"
-fi
-
-# Build protobuf using CMake (autotools no longer available in modern protobuf)
-SAVED_CFLAGS="$CFLAGS"
-SAVED_CXXFLAGS="$CXXFLAGS"
-unset CFLAGS CXXFLAGS
-mkdir -p $SRC/protobuf-build
-cd $SRC/protobuf-build
-cmake $SRC/protobuf \
-    -Dprotobuf_BUILD_TESTS=OFF \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$SRC/protobuf-install \
-    -DBUILD_SHARED_LIBS=OFF
-make -j$(nproc)
-make install
-export CFLAGS="$SAVED_CFLAGS"
-export CXXFLAGS="$SAVED_CXXFLAGS"
-
-export PROTOC="$SRC/protobuf-install/bin/protoc"
-
-# Build protobuf-c using autotools
-# Point it to the protobuf cmake install using explicit flags
 cd $SRC/protobuf-c/
 ./autogen.sh
-PKG_CONFIG_PATH="$SRC/protobuf-install/lib/pkgconfig" PROTOBUF_CFLAGS="-I$SRC/protobuf-install/include" PROTOBUF_LIBS="-L$SRC/protobuf-install/lib -lprotobuf" \
 ./configure --enable-static=yes --enable-shared=false
 
 make -j$(nproc)
 make install
 
-cd $SRC/fuzzing-headers/
-./install.sh
+# Generate C code from proto file
+cd $SRC/protobuf-c/
+protoc --c_out=. -I. -I/usr/local/include t/test-full.proto
 
-cd $SRC/protobuf-c-fuzzers/
-cp $SRC/protobuf-c/t/test-full.proto $SRC/protobuf-c-fuzzers/
-export PATH=$PATH:$SRC/protobuf-c/protoc-c
-$PROTOC --c_out=. -I. -I/usr/local/include test-full.proto
-$CC $CFLAGS test-full.pb-c.c -I $SRC/protobuf-install/include -I $SRC/protobuf-c -c -o test-full.pb-c.o
-$CXX $CXXFLAGS fuzzer.cpp -I $SRC/protobuf-install/include -I $SRC/protobuf-c test-full.pb-c.o $SRC/protobuf-c/protobuf-c/.libs/libprotobuf-c.a $LIB_FUZZING_ENGINE -o $OUT/fuzzer
+# Build the fuzzer
+$CC $CFLAGS -I. -I/usr/local/include -c t/test-full.pb-c.c -o test-full.pb-c.o
+$CC $CFLAGS -I. -I/usr/local/include $SRC/unpack_fuzzer.c test-full.pb-c.o \
+    protobuf-c/.libs/libprotobuf-c.a $LIB_FUZZING_ENGINE -o $OUT/fuzzer
